c24e47499bd3cd90f284711ea2a601654344eca5
David Okaliwe <okaliwedavid@gmail.com>
2023-08-20 21:13:42 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,10 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center 
+    py-5 fixed top-0 z-20 bg-primary`}>
+      
+    </nav>
   )
 }
 
