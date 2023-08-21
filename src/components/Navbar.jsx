9bd8e0d2115d5fd970ee27311c8e8db5d79f171a
David Okaliwe <okaliwedavid@gmail.com>
2023-08-21 13:21:50 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,12 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' 
           className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold 
+          cursor-pointer'
+          >
+            David 
+            <span className='sm:block hidden'> Okaliwe </span>
+          </p>
         </Link>
       </div>
     </nav>
