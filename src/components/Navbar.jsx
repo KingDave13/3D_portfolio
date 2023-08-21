61f8cb2a24d9617a77c176e5c941976fda82df66
David Okaliwe <okaliwedavid@gmail.com>
2023-08-21 14:00:05 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,6 +40,7 @@ const Navbar = () => {
                 : 'text-secondary'
               } hover:text-white text-[18px] font-medium 
                 cursor-pointer`}
+              onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
