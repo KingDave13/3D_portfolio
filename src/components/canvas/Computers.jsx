4a766f1ddc69824ae94f004d9564f89dd867ccf5
David Okaliwe <okaliwedavid@gmail.com>
2023-08-22 12:41:01 +0100

    Update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,33 @@
-import { Suspense, useEffect, useState} from 'react';
-
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { CanvasLoader } from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+    frameloop='demand'
+    shadows
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+
+      </Suspense>
+    </Canvas>
   )
 }
 
