981b50e79c051fe3a8425c96761d7b9b6115a277
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 22:14:08 +0100

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
   };
 
   const contentContainerStyle = {
-    maxHeight: isContentExpanded ? 'none' : '200px',
+    maxHeight: isContentExpanded ? 'none' : '180px',
     overflow: 'hidden',
     transition: 'max-height 0.3s ease-in-out'
   };
@@ -52,39 +52,44 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className='text-secondary text-[17px] max-w-7xl leading-[30px]'
-        style={contentContainerStyle}
-      >
-        I'm a versatile professional with extensive experience in 
-        software development, graphic design and civil engineering. 
-        Being a self-driven learner, I excel at leveraging modern 
-        technologies to create visually striking web applications with 
-        seamless user experiences. My expertise encompasses HTML5, CSS3, 
-        Sass (SCSS), JavaScript, React, Next.js and API integration. 
-        I'm deeply familiar with prevailing development trends and 
-        continually seek to expand my knowledge. <br></br>
-        In backend engineering, I adeptly work with technologies like 
-        Node.js and Express, collaborating with databases such as 
-        MongoDB and PostgreSQL to develop robust and swift APIs. 
-        <br></br><br></br>
-        If you're seeking an experienced frontend or backend developer to 
-        contribute to a team focused on creating cutting-edge digital 
-        products, I'm eager to discuss how I can help you achieve your 
-        goals. <br></br>
-        Please feel free to reach out to me to initiate this conversation 
-        or ask any questions you may have.
-      </motion.p>
+      <div className='max-w-7xl'>
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className='text-secondary text-[17px] 
+          leading-[30px]'
+          style={contentContainerStyle}
+        >
+          I'm a versatile professional with extensive experience in 
+          software development, graphic design and civil engineering. 
+          Being a self-driven learner, I excel at leveraging modern 
+          technologies to create visually striking web applications with 
+          seamless user experiences. My expertise encompasses HTML5, CSS3, 
+          Sass (SCSS), JavaScript, React, Next.js and API integration. 
+          I'm deeply familiar with prevailing development trends and 
+          continually seek to expand my knowledge. <br /><br />
+          In backend engineering, I adeptly work with technologies like 
+          Node.js and Express, collaborating with databases such as 
+          MongoDB and PostgreSQL to develop robust and swift APIs. <br /><br />
+          If you're seeking an experienced frontend or backend developer to 
+          contribute to a team focused on creating cutting-edge digital 
+          products, I'm eager to discuss how I can help you achieve your 
+          goals. <br /><br />
+          Please feel free to reach out to me to initiate this conversation 
+          or ask any questions you may have.
+        </motion.p>
 
-      <button
-        id='readMoreButton'
-        className='text-blue-500 hover:underline focus:outline-none 
-        cursor-pointer'
-        onClick={toggleContent}
-      >
-        {isContentExpanded ? 'Read Less' : 'Read More'}
-      </button>
+        <div className='flex justify-start'>
+          <button
+            id='readMoreButton'
+            className='text-green-500 hover:underline focus:outline-none 
+            cursor-pointer'
+            onClick={toggleContent}
+          >
+            {isContentExpanded ? 'Read Less' : 'Read More'}
+          </button>
+        </div>
+      </div>
+
 
       <div className='mt-20 flex lg:justify-between sm:gap-7 flex-wrap 
       gap-10'>
