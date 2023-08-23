9a22ce5133bc1ebb9d8d3db3e34ff8e8c3620c8b
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 17:32:13 +0100

    Update

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,11 @@
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { staggerContainer } from '../utils/motion';
+
+const SectionWrapper = () => {
+  return (
+    <div>SectionWrapper</div>
+  )
+}
+
+export default SectionWrapper;
