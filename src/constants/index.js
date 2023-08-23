4661cf9e10f714f4ff51e9df4e45ac942a7b37ca
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 16:50:07 +0100

    Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Graphic Designer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Motion Designer",
       icon: creator,
     },
   ];
