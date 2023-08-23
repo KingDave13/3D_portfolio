eefb389ad9796bbae584a72fdd984f7efe04f218
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 16:42:35 +0100

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,6 +16,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
+        className='text-secondary text-[17px] max-w-7xl leading-[30px]'
       >
         I'm a versatile professional with extensive experience in 
         software development, graphic design and civil engineering. 
@@ -24,7 +25,7 @@ const About = () => {
         seamless user experiences. My expertise encompasses HTML5, CSS3, 
         Sass (SCSS), JavaScript, React, Next.js and API integration. 
         I'm deeply familiar with prevailing development trends and 
-        continually seek to expand my knowledge. <br></br><br></br>
+        continually seek to expand my knowledge. <br></br>
         In backend engineering, I adeptly work with technologies like 
         Node.js and Express, collaborating with databases such as 
         MongoDB and PostgreSQL to develop robust and swift APIs. 
@@ -32,7 +33,7 @@ const About = () => {
         If you're seeking an experienced frontend or backend developer to 
         contribute to a team focused on creating cutting-edge digital 
         products, I'm eager to discuss how I can help you achieve your 
-        goals. <br></br><br></br>
+        goals. <br></br>
         Please feel free to reach out to me to initiate this conversation 
         or ask any questions you may have.
       </motion.p>
