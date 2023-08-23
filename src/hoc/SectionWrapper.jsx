9169832248c7ee0059877cff20683e3341ea0fb0
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 17:47:49 +0100

    Update

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -9,8 +9,12 @@ const SectionWrapper = (Component, idName) => function HOC() {
             initial='hidden'
             whileInView='show'
             viewport={{ once: true, amount: 0.25}}
-            className=''
+            className={`${styles.padding} max-w-7xl mx-auto relative
+            z-0`}
         >
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
             <Component />
         </motion.section>
     )
