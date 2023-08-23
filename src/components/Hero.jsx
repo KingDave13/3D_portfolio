28b92fc21c940f0441bedfda53c8a497b9f2b9d6
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 13:58:40 +0100

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,6 +26,18 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex
+      justify-center items-center'
+      >
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 
+          border-secondary flex justify-center items-start p-2'
+          >
+
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
