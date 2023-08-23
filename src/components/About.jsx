4e4d701daaf52231780bb5fa2e5d16aa2a43f869
David Okaliwe <okaliwedavid@gmail.com>
2023-08-23 16:57:59 +0100

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,20 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <p>{title}</p>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px]
+        shadow-card'
+      >
+        <div options={{ max: 45, scale: 1, speed: 450 }}
+          className='bg-tertiary rounded-[20px] py-5 px-12
+          min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+
+        </div>
+      </motion.div>
+    </Tilt>
   )
 };
 
