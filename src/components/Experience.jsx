8cc3e669f09bebbb8f29a4be1a602fb3239e4db9
David Okaliwe <okaliwedavid@gmail.com>
2023-08-24 13:08:30 +0100

    Update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -36,7 +36,12 @@ const ExperienceCard = ({ experience }) => {
 
       <ul className='mt-5 list-disc ml-5 space-y-2'>
         {experience.points.map((point, index) => (
-          
+          <li 
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            
+          </li>
         ))}
       </ul>
     </VerticalTimelineElement>
