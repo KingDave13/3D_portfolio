999df86a7b0383b3b4e4a0049175108598075f26
David Okaliwe <okaliwedavid@gmail.com>
2023-08-24 12:30:04 +0100

    Update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,9 @@ import { VerticalTimeline, VerticalTimelineElement
 import { motion } from 'framer-motion';
 import 'react-vertical-timeline-component/style.min.css';
 import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
