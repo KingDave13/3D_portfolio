22806688590d2242e8a7707e20b311444bb9cd7a
David Okaliwe <okaliwedavid@gmail.com>
2023-08-25 22:27:57 +0100

    Update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -38,9 +38,7 @@ source_code_link }) => {
           </div>
 
           <div className='mt-5'>
-            <h3>
-              
-            </h3>
+            <h3>{name}</h3>
           </div>
       </Tilt>
     </motion.div>
