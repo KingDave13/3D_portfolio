1b6f1783c8b134cbc8b1e5259299c88d202183a7
David Okaliwe <okaliwedavid@gmail.com>
2023-08-25 22:48:13 +0100

    Update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -38,7 +38,20 @@ source_code_link }) => {
           </div>
 
           <div className='mt-5'>
-            <h3>{name}</h3>
+            <h3 className='text-white font-bold text-[24px]'>
+              {name}
+            </h3>
+            <p className='mt-2 text-secondary text-[14px]'>
+              {description}
+            </p>
+          </div>
+
+          <div className='mt-4 flex flex-wrap gap-2'>
+            {tags.map((tag) => (
+              <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
           </div>
       </Tilt>
     </motion.div>
