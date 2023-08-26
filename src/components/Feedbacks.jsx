b1be08261d69930f694d6506f74d46d38d55e22a
David Okaliwe <okaliwedavid@gmail.com>
2023-08-26 23:10:45 +0100

    Update

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,8 +6,12 @@ import { testimonials } from '../constants';
 
 const FeedbackCard = ({ 
   index, testimonial, name, designation, company, image }) => (
-    <motion.div>
-      
+    <motion.div
+      variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+      className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    >
+      <p className='text-white font-black text-[48px]'>"</p>
+      <p>{testimonial}</p>
     </motion.div>
 );
 
