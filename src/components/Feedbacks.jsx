8072067abf7b380c9b4e947bb48ad90ac75bb34e
David Okaliwe <okaliwedavid@gmail.com>
2023-08-26 22:32:20 +0100

    Update

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -22,7 +22,13 @@ const Feedbacks = () => {
 
       <div className={`${styles.paddingX} -mt-20 pb-14 flex 
         flex-wrap gap-7`}>
-          
+          {testimonials.map((testimonial, index) => (
+            <FeedbackCard 
+              key={testimonial.name}
+              index={index}
+              {...testimonial}
+            />
+          ))}
       </div>
     </div>
   );
