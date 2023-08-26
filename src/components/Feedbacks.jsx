2aeb1e25dbe45d7b292ad477837eb5115682d65c
David Okaliwe <okaliwedavid@gmail.com>
2023-08-26 22:28:08 +0100

    Update

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -10,7 +10,10 @@ const Feedbacks = () => {
     <div className='mt-12 bg-black-100 rounded-[20px]'>
       <div className={`${styles.padding} 
         bg-tertiary rounded-2xl min-h-[300px]`}>
-
+          <motion.div variants={textVariant()}>
+            <p>What Others Say</p>
+            <h2>Testimonials.</h2>
+          </motion.div>
       </div>
     </div>
   );
