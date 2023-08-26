c8a66f303ef921eaa50964a6dfcfd18a6f35fff1
David Okaliwe <okaliwedavid@gmail.com>
2023-08-26 22:26:59 +0100

    Update

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,7 +7,12 @@ import { testimonials } from '../constants';
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`${styles.padding} 
+        bg-tertiary rounded-2xl min-h-[300px]`}>
+
+      </div>
+    </div>
   );
 };
 
