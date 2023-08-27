7a5f8e52d57f0c64ba0f9cde0523d8b58f51b48e
David Okaliwe <okaliwedavid@gmail.com>
2023-08-28 00:30:12 +0100

    Update

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,10 +4,12 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF('./planet/scene.gltf');
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive 
+      object={earth.scene} scale={2.5} position-y={0} rotation-y={0} 
+    />
   );
 };
 
@@ -22,7 +24,7 @@ const EarthCanvas = () => {
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6],
+        position: [-4, 3, 6]
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -33,7 +35,6 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
-
         <Preload all />
       </Suspense>
     </Canvas>
