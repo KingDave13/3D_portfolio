a88b19720f8e03afed929e3f29681f920ebd49a4
David Okaliwe <okaliwedavid@gmail.com>
2023-08-27 23:53:11 +0100

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,8 +25,19 @@ const handleSubmit = (e) => {}
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex 
     gap-10 overflow-hidden'>
-      <motion.div variants={slideIn('left', 'tween', 0.2, 1)}>
+      <motion.div variants={slideIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>
+          Get in touch
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Contact.
+        </h2>
 
+        <form>
+          
+        </form>
       </motion.div>
     </div>
   )
