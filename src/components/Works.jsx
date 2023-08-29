f226224a98dbb3b5aae4578ccac0dca07354a028
David Okaliwe <okaliwedavid@gmail.com>
2023-08-29 20:51:30 +0100

    Update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -69,8 +69,8 @@ const Works = () => {
       <div className='w-full flex'>
         <motion.p 
           variants={fadeIn('', '', 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl
-          leading-[30px]'
+          className='mt-3 text-secondary text-[17px] ss:max-w-3xl
+          max-w-6xl leading-[30px]'
         >
           These amazing projects showcase my skill, experience and
           finesse through real world examples. Each project is briefly
