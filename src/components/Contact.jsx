65af388c5e25bec4cd59e302398f83b9a4f80c8d
David Okaliwe <okaliwedavid@gmail.com>
2023-08-31 20:48:35 +0100

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,8 @@ const Modal = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center
      bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-md shadow">
+      <div className="bg-white p-6 rounded-md shadow flex flex-col
+      items-center justify-center">
         <p className="text-center mb-4 text-tertiary">{message}</p>
         <button
           onClick={onClose}
