ef79b621da18675c3fb28ef6e472375cbe139d0e
David Okaliwe <okaliwedavid@gmail.com>
2023-09-01 17:58:13 +0100

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,21 @@ import { SectionWrapper2 } from '../hoc';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { fadeIn, textVariant } from '../utils/motion';
+import { BiCopyright } from 'react-icons/bi';
+
 
 const Footer = () => {
   return (
     <motion.div variants={fadeIn('', 'spring', 0.1)}>
-        <div className='border-t-[1px] border-t-[#fff]'>
-
+        <div className='border-t-[1px] border-t-[#fff] flex items-center
+        justify-center'>
+            <div className='flex mt-5 items-center'>
+                <BiCopyright className='mr-2 text-[20px]'/>
+                <p className='font-poppins font-normal text-center 
+                text-[16px] leading-[27px] text-white'>
+                    2023 David Okaliwe. All Rights Reserved.
+                </p>
+            </div>
         </div>
     </motion.div>
   )
