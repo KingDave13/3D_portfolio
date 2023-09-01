9b3db98c0f88a7bf46d50235cd13ff6734fb7bef
David Okaliwe <okaliwedavid@gmail.com>
2023-09-01 10:56:21 +0100

    Update

---
+++ src/hoc/SocialMedia.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
+
+
+const SocialMedia = () => {
+	return (
+		<div className='app__social'>
+			<div>
+				<BsTwitter />
+			</div>
+			<div>
+				<FaLinkedinIn />
+			</div>
+			<div>
+				<BsInstagram />
+			</div>
+			
+		</div>
+	)
+}
+
+export default SocialMedia;
