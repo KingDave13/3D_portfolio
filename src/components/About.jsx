81d40c3bd79dd642794e9e357997f5386b577bcb
David Okaliwe <okaliwedavid@gmail.com>
2023-09-01 10:43:55 +0100

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>About Me.</h2>
       </motion.div>
 
       <div className='max-w-7xl'>
