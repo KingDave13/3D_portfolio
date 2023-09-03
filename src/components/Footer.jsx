7e108eb0fa658707bef9628a10256120e1972312
David Okaliwe <okaliwedavid@gmail.com>
2023-09-03 13:59:59 +0100

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
     <motion.div variants={fadeIn('', 'spring', 0.1)}>
         <div className='border-t-[1px] border-t-[#fff] flex items-center
         justify-center'>
-            <div className='flex mt-5 items-center'>
-                <BiCopyright className='mr-2 text-[20px]'/>
+            <div className='flex mt-5 sm:items-center'>
+                <BiCopyright className='sm:mr-2 text-[20px]'/>
                 <p className='font-poppins font-medium text-center 
-                text-[16px] leading-[27px] text-white'>
+                sm:text-[16px] text-[15px] leading-[27px] text-white'>
                     2023 David Okaliwe. &nbsp;
                     <span className='font-light'>
                         All Rights Reserved.
@@ -23,12 +23,9 @@ const Footer = () => {
         </div>
 
         <div className='flex flex-row mt-5 items-center justify-center'>
-            {socialMedia.map((social, index, icon) => (
+            {socialMedia.map((social, index) => (
                 <a target='_blank' key={social.id} href={social.link}>
-                    <img 
-                    key={social.id}
-                    src={icon}
-                    alt={social.id}
+                    <social.Icon 
                     className={`w-[21px] h-[21px] object-contain 
                     cursor-pointer ${index !== socialMedia.length -1 ? 
                     'mr-6' : 'mr-0' }`}
